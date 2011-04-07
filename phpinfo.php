e2bcac165786c050bcf5faa2a1c03a44bb33aef6
Eugene Koontz <ekoontz@hiro-tan.org>
2011-04-06 20:07:24 -0700

    change wording a bit

--- phpinfo.php
+++ phpinfo.php
@@ -3,7 +3,7 @@
 head("Content-type:text/html");
 
 print "<h1>Welcome to php end times..</h1>";
-print "<h2>System information:</h2>";
+print "<h2>System information is right here:</h2>";
 
 phpinfo();
 
