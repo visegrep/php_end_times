8c288075e689843dbd7929519b58fd26d3c18dba
visegrep <cookiemonster@.(none)>
2011-04-06 19:14:55 -0700

    your message for you

--- phpinfo.php
+++ phpinfo.php
@@ -4,7 +4,7 @@ head("Content-type:text/html");
 
 print "<h1>Welcome to php end times..</h1>";
 print "<h2>System information is right here:</h2>";
-
+print "<h2>sweet dude how did you do that amazing</h2>"
 phpinfo();
 
 ?>
